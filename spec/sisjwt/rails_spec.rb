b767001158b06de8b516096986716e594824d34d
Andrew Burns <andrew.burns@signinsolutions.com>
2023-02-08 20:54:03 -0700

    Initial commit

---
+++ spec/sisjwt/rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Sisjwt::Rails do
+  it "has a version number" do
+    expect(Sisjwt::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
