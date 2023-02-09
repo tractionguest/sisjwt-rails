760b21f4f01c8368cfb470c322408575ae73fff5
Andrew Burns <andrew.burns@signinsolutions.com>
2023-02-08 23:29:00 -0700

    poc

--- Gemfile
+++ Gemfile
@@ -10,3 +10,6 @@ gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 
 gem "rubocop", "~> 1.21"
+
+gem 'sisjwt', path: "../sisjwt-ruby"
+# gem 'sisjwt', '~> 0.0.1'
--- lib/sisjwt/rails.rb
+++ lib/sisjwt/rails.rb
@@ -1,10 +1,47 @@
 # frozen_string_literal: true
 
-require_relative "rails/version"
+require_relative "version"
+require "active_support/concern"
 
 module Sisjwt
-  module Rails
-    class Error < StandardError; end
-    # Your code goes here...
+  class SisjwtError < StandardError; end
+  module Verification
+    extend ActiveSupport::Concern
+
+    included do
+      before_action :extract_token
+      before_action :authenticate_sisjwt
+
+      def extract_token
+        @token = request.headers['Authorization'][/^Bearer ([^ ]+)$/, 1]
+
+        if @token.blank?
+          head :forbidden
+          return
+        end
+      end
+
+      def authenticate_sisjwt
+        sisjwt = ::Sisjwt::SisJwt.new(sisjwt_options, logger: Rails.logger)
+        @sis_jwt_token = sisjwt.verify(@token)
+      rescue StandardError => e
+        Rails.logger.error("[SISJWT-authenticate_sisjwt]: #{e}")
+        head :forbidden
+      end
+
+      def sisjwt_options
+        return @sisjwt_options if @sisjwt_options.present?
+
+        @sisjwt_options = ::Sisjwt::SisJwtOptions.defaults(mode: :verify).tap do |opts|
+          opts.iss = @@allowed_iss
+        end
+      end
+    end
+
+    class_methods do
+      def sisjwt_iss(allowed_iss)
+        @@allowed_iss = allowed_iss
+      end
+    end
   end
 end
